25ac98fb4ff2d8e14406e7482dfdb2b2670af270
Jude Idoko <judidoko@gmail.com>
2023-09-26 20:08:38 +0100

    Fixing project images not showing bug

--- components/ProjectsSection.jsx
+++ components/ProjectsSection.jsx
@@ -10,7 +10,7 @@ const projectsData = [
     title: " online Bookshop",
     description:
       "A website that enable user to search for a book(s) pay for a period of time and return the book(s) after use",
-    image: "/images/e-book.PNG",
+    image: "./images/e-book.PNG",
     tag: ["All", "React"],
     gitUrl: "https://github.com/judidoko/bookshop",
     previewUrl: "https://emekabookshop.netlify.app/",
@@ -20,7 +20,7 @@ const projectsData = [
     title: "Rental Car Service",
     description:
       "A website where user can search for a car of their choice to rental for period of time",
-    image: "/images/car-Rental.PNG",
+    image: "./images/car-Rental.PNG",
     tag: ["All", "React"],
     gitUrl: "https://github.com/judidoko/Rental-car",
     previewUrl: "https://ija-car-rental.netlify.app/",
@@ -29,7 +29,7 @@ const projectsData = [
     id: 3,
     title: "Movie Box",
     description: "A website that enable user search for movies and watch",
-    image: "/images/movie-box.PNG",
+    image: "./images/movie-box.PNG",
     tag: ["All", "Nextjs"],
     gitUrl: "/",
     previewUrl: "https://hngxmoviebox-stage2.vercel.app/",
@@ -39,7 +39,7 @@ const projectsData = [
     title: "Pig Game",
     description:
       "The Pig is a simple dice game in which two players race to reach 100 points. if a player roll 1 or 0 he loose. 2-6 the number is added the current player score. NOTE: it can only be played on laptop and desktop devices",
-    image: "/images/pig-game.PNG",
+    image: "./images/pig-game.PNG",
     tag: ["All", "JavaScript"],
     gitUrl: "https://github.com/judidoko/pig-game",
     previewUrl: "https://pig-game-paly.netlify.app/",
