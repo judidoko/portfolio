77d38d8f0b08a2379ca42a0658d660a243225b30
Jude Idoko <judidoko@gmail.com>
2023-12-06 14:43:58 +0100

    Add task-mgt-app image

--- components/ProjectsSection.jsx
+++ components/ProjectsSection.jsx
@@ -20,7 +20,7 @@ const projectsData = [
     title: "Task Management App",
     description:
       "This app allow users to easily Create, organize and track theirs tasks or/and Projects.",
-    image: "./images/task-mgt-app.PNG",
+    image: "./images/task-app.PNG",
     tag: ["All", "React"],
     gitUrl: "https://github.com/judidoko/Task-management-app",
     previewUrl: "https://tasks-mgt-app.netlify.app/",
