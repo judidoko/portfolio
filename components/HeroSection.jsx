bcb50dec61bab25bb48f8441985d746945977315
Jude Idoko <judidoko@gmail.com>
2024-02-06 23:33:07 +0100

    Fix bug on the hero section

--- components/HeroSection.jsx
+++ components/HeroSection.jsx
@@ -23,9 +23,9 @@ const HeroSection = () => {
               sequence={[
                 "Jude Idoko",
                 1000,
-                "A Frontend Developer",
+                "a Frontend Developer",
                 1000,
-                "A Web Developer",
+                "a Web Developer",
                 1000,
               ]}
               wrapper="span"
